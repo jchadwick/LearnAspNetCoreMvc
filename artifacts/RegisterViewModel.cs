0401cce338d9482dbf3099a8dafbd876013f8221
Jess Chadwick <jesschadwick@gmail.com>
2016-11-30 01:19:33 -0500

    Ch06/06_03

---
+++ artifacts/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExploreCalifornia.Models.Account
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress, MaxLength(500)]
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Passwords must match")]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
+
+}
