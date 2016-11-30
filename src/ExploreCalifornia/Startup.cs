58f7125ab99af48b9bdb44dec4447b3940571f06
Jess Chadwick <jesschadwick@gmail.com>
2016-11-30 01:18:53 -0500

    Ch01/01_03_End

--- src/ExploreCalifornia/Startup.cs
+++ src/ExploreCalifornia/Startup.cs
@@ -28,9 +28,21 @@ namespace ExploreCalifornia
                 app.UseDeveloperExceptionPage();
             }
 
+
+            app.Use(async (context, next) =>
+            {
+                if(context.Request.Path.Value.StartsWith("/hello"))
+                {
+                    await context.Response.WriteAsync("Hello ASP.NET Core!");
+                }
+
+                await next();
+            });
+
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello ASP.NET Core!");
+                await context.Response.WriteAsync(" How are you?");
             });
         }
     }
