3ac0bfb3d00f4c9cef10c6517fe933ff9c8488bd
Jess Chadwick <jesschadwick@gmail.com>
2016-11-30 01:18:56 -0500

    Ch01/01_07_End

--- src/ExploreCalifornia/Startup.cs
+++ src/ExploreCalifornia/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace ExploreCalifornia
 {
@@ -25,7 +26,11 @@ namespace ExploreCalifornia
 
             app.UseExceptionHandler("/error.html");
 
-            if (env.IsDevelopment())
+            var configuration = new ConfigurationBuilder()
+                                    .AddEnvironmentVariables()
+                                    .Build();
+
+            if (configuration.GetValue<bool>("EnableDeveloperExceptions"))
             {
                 app.UseDeveloperExceptionPage();
             }
