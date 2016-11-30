69d2e529354f40e9db97a0117818268038d47f8c
Jess Chadwick <jesschadwick@gmail.com>
2016-11-30 01:19:24 -0500

    Ch05/05_01_End

---
+++ src/ExploreCalifornia/wwwroot/scripts/site.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+
+    $('#mainContent').on('click', '.pager a', function () {
+        var url = $(this).attr('href');
+
+        $('#mainContent').load(url);
+
+        return false;
+    })
+
+})
