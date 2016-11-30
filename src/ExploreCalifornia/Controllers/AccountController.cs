6dabef2c6b9088477a4c3bac8899b043aef7e794
Jess Chadwick <jesschadwick@gmail.com>
2016-11-30 01:19:34 -0500

    Ch06/06_03_End

--- src/ExploreCalifornia/Controllers/AccountController.cs
+++ src/ExploreCalifornia/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using ExploreCalifornia.Models.Account;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -10,13 +11,20 @@ namespace ExploreCalifornia.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AccountController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Register()
         {
             return View(new RegisterViewModel());
         }
 
         [HttpPost]
-        public IActionResult Register(RegisterViewModel registration)
+        public async Task<IActionResult> Register(RegisterViewModel registration)
         {
             if (!ModelState.IsValid)
                 return View(registration);
@@ -27,7 +35,15 @@ namespace ExploreCalifornia.Controllers
                 UserName = registration.EmailAddress,
             };
 
-            // TODO:  Finish registering the user
+            var result = await _userManager.CreateAsync(newUser, registration.Password);
+
+            if(!result.Succeeded)
+            {
+                ModelState.AddModelError("", "Error creating new user");
+                return View();
+            }
+
+            return RedirectToAction("Login");
         }
     }
 }
