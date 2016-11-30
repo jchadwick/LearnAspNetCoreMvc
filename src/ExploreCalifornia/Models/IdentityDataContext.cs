fce58347a5e8cfcb67597c32c3ad183a072401c2
Jess Chadwick <jesschadwick@gmail.com>
2016-11-30 01:19:32 -0500

    Ch06/06_02_End

---
+++ src/ExploreCalifornia/Models/IdentityDataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExploreCalifornia.Models
+{
+    public class IdentityDataContext : IdentityDbContext<IdentityUser>
+    {
+        public IdentityDataContext(DbContextOptions<IdentityDataContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
