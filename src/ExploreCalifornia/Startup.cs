04f4a1324d5fb6f5b176c3ca97700281e1a8ed45
Jess Chadwick <jesschadwick@gmail.com>
2016-11-30 01:19:12 -0500

    Ch03/03_09_End

--- src/ExploreCalifornia/Startup.cs
+++ src/ExploreCalifornia/Startup.cs
@@ -29,6 +29,8 @@ namespace ExploreCalifornia
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<SpecialsDataContext>();
+
             services.AddTransient<FormattingService>();
 
             services.AddTransient<FeatureToggles>(x => new FeatureToggles
--- src/ExploreCalifornia/ViewComponents/MonthlySpecialsViewComponent.cs
+++ src/ExploreCalifornia/ViewComponents/MonthlySpecialsViewComponent.cs
@@ -10,9 +10,17 @@ namespace ExploreCalifornia.ViewComponents
     [ViewComponent]
     public class MonthlySpecialsViewComponent : ViewComponent
     {
+        private readonly SpecialsDataContext _specials;
+
+        public MonthlySpecialsViewComponent(SpecialsDataContext specials)
+        {
+            _specials = specials;
+        }
+
         public IViewComponentResult Invoke()
         {
-            return View();
+            var specials = _specials.GetMonthlySpecials();
+            return View(specials);
         }
 
     }
