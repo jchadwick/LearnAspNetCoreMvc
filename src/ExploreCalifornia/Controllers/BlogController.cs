ef4b9cf5d6243a1b94af6fe7934d703a74d49e0b
Jess Chadwick <jesschadwick@gmail.com>
2016-11-30 01:19:07 -0500

    Ch03/03_04_End

--- src/ExploreCalifornia/Controllers/BlogController.cs
+++ src/ExploreCalifornia/Controllers/BlogController.cs
@@ -20,6 +20,11 @@ namespace ExploreCalifornia.Controllers
         [Route("{year:min(2000)}/{month:range(1,12)}/{key}")]
         public IActionResult Post(int year, int month, string key)
         {
+            ViewBag.Title = "My blog post";
+            ViewBag.Posted = DateTime.Now;
+            ViewBag.Author = "Jess Chadwick";
+            ViewBag.Body = "This is a great blog post, don't you think?";
+
             return View();
         }
     }
