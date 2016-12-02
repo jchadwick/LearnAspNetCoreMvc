5aadae186983af846f1fd85d551b407cf58e52dc
Jess Chadwick <jesschadwick@gmail.com>
2016-12-02 00:25:24 -0500

    Ch03/03_03_End

--- src/ExploreCalifornia/Controllers/BlogController.cs
+++ src/ExploreCalifornia/Controllers/BlogController.cs
@@ -14,17 +14,13 @@ namespace ExploreCalifornia.Controllers
         [Route("")]
         public IActionResult Index()
         {
-            return new ContentResult { Content = "Blog posts" };
+            return View();
         }
 
         [Route("{year:min(2000)}/{month:range(1,12)}/{key}")]
         public IActionResult Post(int year, int month, string key)
         {
-            return new ContentResult
-            {
-                Content = string.Format("Year: {0};  Month: {1};  Key: {2}",
-                                        year, month, key)
-            };
+            return View();
         }
     }
 }
