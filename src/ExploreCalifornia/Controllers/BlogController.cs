928fd7c66d0f3daabfe5f7f3758659ae325ad8c3
Jess Chadwick <jesschadwick@gmail.com>
2016-12-02 00:25:50 -0500

    Ch05/05_01_End

--- src/ExploreCalifornia/Controllers/BlogController.cs
+++ src/ExploreCalifornia/Controllers/BlogController.cs
@@ -22,7 +22,26 @@ namespace ExploreCalifornia.Controllers
         [Route("")]
         public IActionResult Index(int page = 0)
         {
-            var posts = _db.Posts.OrderByDescending(x => x.Posted).Take(5).ToArray();
+            var pageSize = 2;
+            var totalPosts = _db.Posts.Count();
+            var totalPages = totalPosts / pageSize;
+            var previousPage = page - 1;
+            var nextPage = page + 1;
+
+            ViewBag.PreviousPage = previousPage;
+            ViewBag.HasPreviousPage = previousPage >= 0;
+            ViewBag.NextPage = nextPage;
+            ViewBag.HasNextPage = nextPage < totalPages;
+
+            var posts =
+                _db.Posts
+                    .OrderByDescending(x => x.Posted)
+                    .Skip(pageSize * page)
+                    .Take(pageSize)
+                    .ToArray();
+
+            if (Request.Headers["X-Requested-With"] == "XMLHttpRequest")
+                return PartialView(posts);
 
             return View(posts);
         }
