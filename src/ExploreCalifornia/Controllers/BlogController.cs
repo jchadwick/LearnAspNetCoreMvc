75f16ecc647d4e66acddb8f6cafa8bd372e5a800
Jess Chadwick <jesschadwick@gmail.com>
2016-12-02 00:25:19 -0500

    Ch02/02_06_End

--- src/ExploreCalifornia/Controllers/BlogController.cs
+++ src/ExploreCalifornia/Controllers/BlogController.cs
@@ -8,17 +8,22 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ExploreCalifornia.Controllers
 {
+    [Route("blog")]
     public class BlogController : Controller
     {
-        // GET: /<controller>/
+        [Route("")]
         public IActionResult Index()
         {
             return new ContentResult { Content = "Blog posts" };
         }
 
-        public IActionResult Post(int id = -1)
+        [Route("{year:min(2000)}/{month:range(1,12)}/{key}")]
+        public IActionResult Post(int year, int month, string key)
         {
-            return new ContentResult { Content = id.ToString() };
+            return new ContentResult {
+                Content = string.Format("Year: {0};  Month: {1};  Key: {2}",
+                                        year, month, key)
+            };
         }
     }
 }
