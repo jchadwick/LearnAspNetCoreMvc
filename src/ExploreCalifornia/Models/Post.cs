1dce9aff12437327ea34c741e2010273c21e3696
Jess Chadwick <jesschadwick@gmail.com>
2016-12-02 00:25:44 -0500

    Ch04/04_05_End

--- src/ExploreCalifornia/Models/Post.cs
+++ src/ExploreCalifornia/Models/Post.cs
@@ -2,12 +2,30 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ExploreCalifornia.Models
 {
     public class Post
     {
+        public long Id { get; set; }
+
+        private string _key;
+
+        public string Key
+        {
+            get
+            {
+                if(_key == null)
+                {
+                    _key = Regex.Replace(Title.ToLower(), "[^a-z0-9]", "-");
+                }
+                return _key;
+            }
+            set { _key = value; }
+        }
+
         [Display(Name = "Post Title")]
         [Required]
         [DataType(DataType.Text)]
