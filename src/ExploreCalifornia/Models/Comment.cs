5991eb877c0d8e82345d454e44d92170baed1fd4
Jess Chadwick <jesschadwick@gmail.com>
2016-12-02 00:25:54 -0500

    Ch05/05_03_End

---
+++ src/ExploreCalifornia/Models/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExploreCalifornia.Models
+{
+    public class Comment
+    {
+        public long Id { get; set; }
+
+        public long PostId { get; set; }
+        public virtual Post Post { get; set; }
+
+        public DateTime Posted { get; set; }
+        public string Author { get; set; }
+
+        [Required]
+        public string Body { get; set; }
+    }
+}
