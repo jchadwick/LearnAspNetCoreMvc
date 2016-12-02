b1ae3629c31f9e37df54cd2fef29e2b760178273
Jess Chadwick <jesschadwick@gmail.com>
2016-12-02 00:25:40 -0500

    Ch04/04_03_End

--- src/ExploreCalifornia/Models/Post.cs
+++ src/ExploreCalifornia/Models/Post.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,20 @@ namespace ExploreCalifornia.Models
 {
     public class Post
     {
+        [Display(Name = "Post Title")]
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(100, MinimumLength = 5,
+            ErrorMessage = "Title must be between 5 and 100 characters long")]
         public string Title { get; set; }
+
         public string Author { get; set; }
+
+        [Required]
+        [MinLength(100, ErrorMessage = "Blog posts must be at least 100 characters long")]
+        [DataType(DataType.MultilineText)]
         public string Body { get; set; }
+
         public DateTime Posted { get; set; }
     }
 }
