d935426d11853c98d772825766882cfd06a74fb4
Jess Chadwick <jesschadwick@gmail.com>
2016-12-02 00:25:32 -0500

    Ch03/03_08_End

---
+++ src/ExploreCalifornia/ViewComponents/MonthlySpecialsViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using ExploreCalifornia.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExploreCalifornia.ViewComponents
+{
+    [ViewComponent]
+    public class MonthlySpecialsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
