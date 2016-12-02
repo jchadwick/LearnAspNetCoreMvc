6107e45c793a627880df78a26c18a64e4e82161c
Jess Chadwick <jesschadwick@gmail.com>
2016-12-02 00:26:04 -0500

    Ch06/06_04_End

--- src/ExploreCalifornia/Controllers/AccountController.cs
+++ src/ExploreCalifornia/Controllers/AccountController.cs
@@ -11,12 +11,59 @@ namespace ExploreCalifornia.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly SignInManager<IdentityUser> _signinManager;
         private readonly UserManager<IdentityUser> _userManager;
 
-        public AccountController(UserManager<IdentityUser> userManager)
+        public AccountController(
+                UserManager<IdentityUser> userManager,
+                SignInManager<IdentityUser> signinManager
+            )
         {
             _userManager = userManager;
+            _signinManager = signinManager;
         }
+
+        public IActionResult Login()
+        {
+            return View(new LoginViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel login, string returnUrl = null)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var result = await _signinManager.PasswordSignInAsync(
+                login.EmailAddress, login.Password,
+                login.RememberMe, false
+            );
+
+            if(!result.Succeeded)
+            {
+                ModelState.AddModelError("", "Login error!");
+                return View();
+            }
+
+            if (string.IsNullOrWhiteSpace(returnUrl))
+                return RedirectToAction("Index", "Home");
+
+            return Redirect(returnUrl);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout(string returnUrl = null)
+        {
+            await _signinManager.SignOutAsync();
+
+            if (string.IsNullOrWhiteSpace(returnUrl))
+                return RedirectToAction("Index", "Home");
+
+            return Redirect(returnUrl);
+        }
+
 
         public IActionResult Register()
         {
