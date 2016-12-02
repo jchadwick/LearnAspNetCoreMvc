ab83035b939681f01a6cae2387c096003037feb5
Jess Chadwick <jesschadwick@gmail.com>
2016-12-02 00:26:02 -0500

    Ch06/06_03

---
+++ artifacts/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using ExploreCalifornia.Models.Account;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExploreCalifornia.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View(new RegisterViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel registration)
+        {
+            if (!ModelState.IsValid)
+                return View(registration);
+
+            var newUser = new IdentityUser
+            {
+                Email = registration.EmailAddress,
+                UserName = registration.EmailAddress,
+            };
+
+            // TODO:  Finish registering the user
+        }
+    }
+}
